b74173fa1f909cb110c890451ee34be6f0ba5b60
Mohammed Saeed <116583984+Mohammed0Saeed@users.noreply.github.com>
2025-04-27 13:31:58 +0200

    Update bitcoin.py to work the api v3 with a key request

--- week4/bitcoin.py
+++ week4/bitcoin.py
@@ -7,15 +7,16 @@ if len(sys.argv) == 1:
 # the correct input is one number next to the command
 elif len(sys.argv) == 2:
     try:
-      bitcoinJson = requests.get("https://api.coindesk.com/v1/bpi/currentprice.json")
+      bitcoinJson = requests.get("https://rest.coincap.io/v3/assets/bitcoin?apiKey=42db4378d4e3f4aa4c32ad5984b20e3b050642d5699d39be0fade34cd6cfbc30")
+      #currentprice.json
     # in case that an error with request occured, close the program safely
     except requests.RequestException:
       sys.exit("Invalid Request")
     else:
       pass
-    
+
     # get the price of the bitcoin from the request link
-    price = float(bitcoinJson.json()["bpi"]["USD"]["rate"].replace(",", ""))
+    price = float(bitcoinJson.json()["data"]["priceUsd"].replace(",", ""))
     try:
       amount = price * float(sys.argv[1])
     # to check for if the input is a float number
@@ -23,4 +24,3 @@ elif len(sys.argv) == 2:
        sys.exit("Command-line argument is not a number")
     else:
       print(f"${amount:,.4f}")
-
